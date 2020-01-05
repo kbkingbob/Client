9c7f300affe369db5965f26c6a262eead9c58048
kuliangce <925801579@qq.com>
2020-01-06 03:25:25 +0800

    修改关卡制作器

--- Assets/Makercode/End.cs
+++ Assets/Makercode/End.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public class End : MonoBehaviour
 {
-    GameObject[] sno1;
-    string txt;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,20 +19,21 @@ public class End : MonoBehaviour
     }
     public void Button()
     {
-        StreamWriter F = new StreamWriter("names.txt");
-        sno1 = GameObject.FindGameObjectsWithTag("Block");
+        GameObject[] sno1;
+        string txt = "";
+        Text names = GameObject.Find("AudioPanel").GetComponent<MakerAudioPlayer>().audioName;
+        sno1 = GameObject.FindGameObjectsWithTag("Fire");
         foreach(GameObject item in sno1)
         {
             int type = -1;
-            if (item.name == "New Sprite (1)(Clone)") type = 1;
-            else if (item.name == "New Sprite (2)(Clone)") type = 2;
-            else if (item.name == "Enemy(Clone)") type = 3;
+            if (item.name == "Roadblock(Clone)") type = 1;
+            else if (item.name == "Plane(Clone)") type = 2;
+            else if (item.name == "EnemyAll(Clone)") type = 3;
             float a = item.transform.localPosition.x;
-            float b = item.transform.localPosition.y;
-            float c = item.transform.localPosition.z;
-            string str = type.ToString() + " " + a.ToString() + " " + b.ToString() + " " + c.ToString();
+            string str = type.ToString() + " " + a.ToString();
             if (type != -1)txt += str + "\n";
          }
+        StreamWriter F = new StreamWriter(names.text + ".txt", false);
         F.Write(txt);
         F.Close();
     }
